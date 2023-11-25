1c6b19aaa28bfb17bba247c3341bb9014455f73c
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2023-11-25 09:52:14 +0530

    DVC added

---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = [
+    {"name":"Rishav", "age":24, "city":"patna"}
+    {"name":"subham", "age":25, "city":"delhi"}
+    {"name":"sunny", "age":27, "city":"bhopal"}
+]
+
+df = pd.DataFrame(data)
+
+df.to_csv("data/data.csv", index=False)
